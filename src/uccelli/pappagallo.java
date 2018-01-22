661136cb8d6369a2b7befd815c2f72d5a0d84c80
RicciLorenzo <lorenzorik@gmail.com>
2018-01-22 17:19:24 +0100

    update

--- src/uccelli/pappagallo.java
+++ src/uccelli/pappagallo.java
@@ -4,14 +4,30 @@ public class pappagallo {
 
 	String nome;
 	int anni;
-	
+	int costo=20;
 	
 	public void cambiaNome(String nome) {
 		this.nome=nome;
 	}
 	
+	public void cambiaAnni(int anni) {
+		this.anni=anni;
+	}
+	
 	public void printNome() {
-		System.out.println(this.nome);
+		System.out.println("Nome: "+this.nome);
+	}
+	
+	public void printAnni() {
+		System.out.println("Anni: "+this.anni);
+	}
+	
+	public void possoComprarlo(int euro) {
+		System.out.println("Posso comprarlo con "+euro+"euro?");
+		if(euro>=this.costo)
+			System.out.println("Si");
+		else
+			System.out.println("No");
 	}
 	
 	
