c6031dc6d76449d458984d55eaac02bf33c5a796
RicciLorenzo <lorenzorik@gmail.com>
2018-01-22 16:33:38 +0100

    update

--- src/extend/gatto.java
+++ src/extend/gatto.java
@@ -8,7 +8,7 @@ public class gatto {
 	
 	
 	public void printNome() {
-		System.out.println("Nome del gattolol: "+this.nome);
+		System.out.println("Nome del gatto: "+this.nome);
 	}
 	
 	public void printRazza() {
