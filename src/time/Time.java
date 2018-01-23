ee4a35d54903a0bd72ee4df6ae6cc240e2d7bf33
RicciLorenzo <lorenzorik@gmail.com>
2018-01-24 00:19:09 +0100

    update

---
+++ src/time/Time.java
@@ -0,0 +1,19 @@
+package time;
+
+public class Time implements Comparable<Time> {
+
+	@Override
+	public int compareTo(Time arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Time(int year, int month, int day, int hours, int minutes) {
+		
+		
+		
+	}
+	
+	
+	
+}
