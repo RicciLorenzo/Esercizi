5c084050249d4c975f2582abd15270266e6e7810
RicciLorenzo <lorenzorik@gmail.com>
2018-01-24 00:20:36 +0100

    update

--- src/time/Time.java
+++ src/time/Time.java
@@ -2,16 +2,24 @@ package time;
 
 public class Time implements Comparable<Time> {
 
+	public Time(int year, int month, int day, int hours, int minutes) {
+		
+		
+		
+	}
+	
+	
 	@Override
 	public int compareTo(Time arg0) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	
-	public Time(int year, int month, int day, int hours, int minutes) {
+	public String toStringOnlyHour() {
 		
+		//... ritorna stringhe del tipo hh:mm (solo ore e minuti) 
 		
-		
+		return null;
 	}
 	
 	
