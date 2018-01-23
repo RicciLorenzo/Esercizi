34f74222e9abe1c4e031924b265e72460d45148c
RicciLorenzo <lorenzorik@gmail.com>
2018-01-24 00:39:40 +0100

    update

--- src/time/Time.java
+++ src/time/Time.java
@@ -1,25 +1,52 @@
 package time;
 
 public class Time implements Comparable<Time> {
-
+	
+	private int year;
+	private int month;
+	private int day;
+	private int hours;
+	private int minutes;
+	
 	public Time(int year, int month, int day, int hours, int minutes) {
 		
-		
+		this.year=year;
+		this.month=month;
+		this.day=day;
+		this.hours=hours;
+		this.minutes=minutes;
 		
 	}
 	
 	
 	@Override
 	public int compareTo(Time arg0) {
+		
 		// TODO Auto-generated method stub
+		// Restituisce 0 se sono uguali; minore di 0 se "other" é una data maggiore; ritorna maggiore di 0 se this é maggiore.
+		
+		
+		
 		return 0;
 	}
 	
 	public String toStringOnlyHour() {
 		
-		//... ritorna stringhe del tipo hh:mm (solo ore e minuti) 
+		//... ritorna stringhe del tipo hh:mm (solo ore e minuti)		
 		
-		return null;
+		/*if(this.minutes==00 && this.hours==00)
+			return "00:00";
+		
+		if(this.hours==00) 
+			return "00:"+this.minutes;
+		
+		if(this.minutes==00)
+			return this.hours+":00";
+		
+		return this.hours+":"+this.minutes;*/
+		
+		return String.format("%02d:%02d", this.hours,this.minutes);		//VERSIONE PRO		
+		
 	}
 	
 	
--- src/time/main.java
+++ src/time/main.java
@@ -4,6 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
+		Time tempo = new Time(2018,1,24,00,28);
+		System.out.println(tempo.toStringOnlyHour());
+		
 	}
 	
 }
