a63b6ce6f8787a47d7a982327c784751da4ce711
RicciLorenzo <lorenzorik@gmail.com>
2018-01-23 23:29:20 +0100

    update

---
+++ src/time/time.java
@@ -0,0 +1,15 @@
+package time;
+
+import java.sql.Time;
+
+public class time implements Comparable<Time> {
+
+	@Override
+	public int compareTo(Time arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+}
