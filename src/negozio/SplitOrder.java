0e868d97f3b6a26b32ceb1d3d62a80de0007ed45
RicciLorenzo <lorenzorik@gmail.com>
2018-01-26 01:25:27 +0100

    update

--- src/negozio/SplitOrder.java
+++ src/negozio/SplitOrder.java
@@ -1,5 +1,10 @@
 package negozio;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SplitOrder extends Order {
 	
 	public SplitOrder(Shop shop, Product... products) {
@@ -11,6 +16,20 @@ public class SplitOrder extends Order {
 		//...compra i prodotti e ritorna una o piu’ spedizioni 
 		
 		this.buy();
+		
+		Map<Integer, List<Product>> spedizioni = new HashMap<>();
+		for(Product product : getProducts()) {
+			int day=product.getDaysBeforeShipping();
+			spedizioni.putIfAbsent(day, new ArrayList<>());
+			spedizioni.get(day).add(product);
+		}
+		
+		ArrayList<Shipping> allSplitted = new ArrayList<>();
+		for(int a : spedizioni.keySet()) {
+			allSplitted.add(new Shipping(spedizioni.get(a)));
+		}
+		
+		return allSplitted;
 		
 	} 
 	
