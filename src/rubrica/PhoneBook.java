ccaf232869ac903aeba6b649a9815f6b6f9cd38f
RicciLorenzo <lorenzorik@gmail.com>
2018-01-30 23:04:08 +0100

    update

---
+++ src/rubrica/PhoneBook.java
@@ -0,0 +1,78 @@
+package rubrica;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class PhoneBook extends View {
+	
+	List<Entry> rubrica = new ArrayList<>();
+			
+	public static class Entry {
+		
+		public final String name; 
+		public final String surname; 
+		public final int phone; 
+		public final boolean sex; 
+		public final static boolean MALE = false, FEMALE = true;
+		
+	
+	
+	private Entry(String name, String surname, int phone, boolean sex) {
+		
+		this.name = name; 
+		this.surname = surname; 
+		this.phone = phone; 
+		this.sex = sex; 
+		
+	}
+
+	
+	@Override 
+	public String toString() {
+		return name + " " + surname + ": " + phone + (sex == MALE ? " [male]" : " [female]"); 
+	}
+	
+}
+	
+	
+	public void add(String name, String surname, int phone, boolean sex) {
+		// aggiunge l’entry; se gia’ ne esisteva una con lo stesso nome e cognome, la sostituisce 
+
+		for(Entry contatto : rubrica) {
+			if(rubrica.contains(name)&&rubrica.contains(surname)) {
+				System.out.println("Nome e cognome giá presenti; sostituzione dei dati...");
+				contatto.name=name;
+				contatto.surname=surname;
+				contatto.phone=phone;
+				contatto.sex=sex;
+			}			
+		}
+		
+		rubrica.add(new Entry(name,surname,phone,sex));
+		
+	}
+	
+	
+	public void remove(String name, String surname) {
+		// rimuove l’entry con tale nome e cognome; se non e’ presente, lancia una UnknownEntryException 
+		
+		for(Entry contatto : rubrica) {
+			if(contatto.name==name && contatto.surname==surname)
+				rubrica.remove(contatto);
+		}
+		
+		if(!(rubrica.contains(surname)&&rubrica.contains(name))) {
+			throw new UnknowEntryException();
+		}
+		
+	}
+		
+	@Override 
+	public Iterator<Entry> iterator() {
+		// resituisce un iteratore sulle entry di questo PhoneBook 
+		
+		return rubrica.iterator();
+	}
+	
+}
