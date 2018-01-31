acff09c1f988b539800ba44aa31567c83f2c632e
RicciLorenzo <lorenzorik@gmail.com>
2018-01-31 16:22:15 +0100

    update

--- src/rubrica/PhoneBook.java
+++ src/rubrica/PhoneBook.java
@@ -1,7 +1,11 @@
 package rubrica;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class PhoneBook extends View { 
 	
+	ArrayList<Entry> rubrica = new ArrayList<>();
 	
 	public static class Entry {
 	public final String name; 
@@ -9,7 +13,7 @@ public class PhoneBook extends View {
 	public final int phone; 
 	public final boolean sex; 
 	public final static boolean MALE = false, FEMALE = true;
-	}
+	
 	
 
 	private Entry(String name, String surname, int phone, boolean sex) {
@@ -27,13 +31,39 @@ public class PhoneBook extends View {
 
 
 public void add(String name, String surname, int phone, boolean sex) {
-	... // aggiunge l’entry; se gia’ ne esisteva una con lo stesso nome e cognome, la sostituisce 
+	// aggiunge l’entry; se gia’ ne esisteva una con lo stesso nome e cognome, la sostituisce
+	
+	for(Entry contatto : rubrica) {
+		if(contatto.name==name && contatto.surname==surname) {
+			rubrica.remove(contatto);
+			rubrica.add(new Entry(name,surname,phone,sex));
+			return;
+		}
+	}
+	
+	rubrica.add(new Entry(name,surname,phone,sex));
+	
 }
 
 public void remove(String name, String surname) {
-	... // rimuove l’entry con tale nome e cognome; se non e’ presente, lancia una UnknownEntryException 
+	// rimuove l’entry con tale nome e cognome; se non e’ presente, lancia una UnknownEntryException
+	
+	for(Entry contatto : rubrica) {
+		if(contatto.name==name && contatto.surname==surname) {
+			rubrica.remove(contatto);
+			return;
+		}
+	}
+	
+	throw new UnknowEntryException();
+	
 }
 
-@Override 
-public Iterator<Entry> iterator() { ... // resituisce un iteratore sulle entry di questo PhoneBook }
+@Override public Iterator<Entry> iterator() {  
+	// resituisce un iteratore sulle entry di questo PhoneBook
+	
+	return rubrica.iterator();
+	
+}
+
 }
