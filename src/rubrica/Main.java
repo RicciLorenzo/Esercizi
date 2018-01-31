820918fea6b3ce085fd089da67828d2382524dc2
RicciLorenzo <lorenzorik@gmail.com>
2018-01-31 22:06:33 +0100

    update

--- src/rubrica/Main.java
+++ src/rubrica/Main.java
@@ -1,28 +1,30 @@
 package rubrica;
 
+import java.util.Comparator;
+
 import rubrica.PhoneBook.Entry;
 import rubrica.PhoneBook.Entry.*;
 
 public class Main {
-	public static void main(String[] args) { 
+	public static void main(String[] args) throws UnknowEntryException { 
 		
 		PhoneBook book = new PhoneBook();
-		book.add("Primo", "Levi", 7569222, MALE); 
-		book.add("Fausto", "Spoto", 1234567, MALE); 
-		book.add("Giorgio", "Levi", 1563423, MALE); 
-		book.add("Catherine", "Bach", 367745, FEMALE);
-		book.add("Pietro", "Ferrara", 7865634, MALE); 
-		book.add("Alberto", "Lovato", 8746728, MALE); 
-		book.add("Thomas", "Scudiero", 453678, MALE); 
-		book.add("Pietro", "Ferrara", 333334, MALE); // sostituisce 
-		book.add("Cybill", "Shepherd", 987567546, FEMALE); 
-		book.add("Audrey", "Hepburn", 32444, FEMALE); 
+		book.add("Primo", "Levi", 7569222, false); 
+		book.add("Fausto", "Spoto", 1234567, false); 
+		book.add("Giorgio", "Levi", 1563423, false); 
+		book.add("Catherine", "Bach", 367745, true);
+		book.add("Pietro", "Ferrara", 7865634, false); 
+		book.add("Alberto", "Lovato", 8746728, false); 
+		book.add("Thomas", "Scudiero", 453678, false); 
+		book.add("Pietro", "Ferrara", 333334, false); // sostituisce 
+		book.add("Cybill", "Shepherd", 987567546, true); 
+		book.add("Audrey", "Hepburn", 32444, true); 
 		book.remove("Fausto", "Spoto");
 		
 		View view = book; 
 		System.out.println("DIRECT VIEW"); 
 		System.out.println(view);
-		view = new SexView(view, MALE); 
+		view = new SexView(view, false); 
 		System.out.println("MALE ONLY VIEW"); 
 		System.out.println(view);
 		Comparator<Entry> comparator = new Comparator<Entry>() { // ordina per cognome e poi per nome
