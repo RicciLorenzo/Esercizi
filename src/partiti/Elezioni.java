318e5a1178b3be2713e4e05aa20b50cb73659303
RicciLorenzo <lorenzorik@gmail.com>
2018-02-06 00:09:47 +0100

    update

--- src/partiti/Elezioni.java
+++ src/partiti/Elezioni.java
@@ -17,18 +17,33 @@ public class Elezioni {
 	public String toString() { 
 		//...sulla base dei voti registrati per ciascun partito, restituisce una stringa del tipo:
 		//Risultati elezioni:
-		//1 PENSIONATI: 7762 voti (33.03%) 2 FELICE: 3216 voti (13.68%) 3 FLOREALE: 5695 voti (24.23%) 4 CAOTICO: 5731 voti (24.39%) 5 BASSOTTI: 1098 voti (4.67%)
+		//1	PENSIONATI: 7762 voti (33.03%) 
+		//2 FELICE: 3216 voti (13.68%) 
+		//3 FLOREALE: 5695 voti (24.23%) 
+		//4 CAOTICO: 5731 voti (24.39%) 
+		//5 BASSOTTI: 1098 voti (4.67%)
 		
+		String result="Risultati elezioni:\n";
+		for(Partito partito :elezioni.keySet() ) {
+			result+=partito.indice()+"\t"+partito.name()+": "+votiPer(partito)+" "+((votiPer(partito)*100)/numeroVotiEspressi());
+		}
 		
+		return result;
 		
 	}
 	
 	protected final int numeroVotiEspressi() { 
 		//...restituisce il numero totale di voti registrati 
+		int tot=0;
+		for(int voti : elezioni.values()) {
+			tot+=voti;
+		}
+		return tot;
 	}
 	
 	protected final int votiPer(Partito partito) {
 		//...restituisce il numero totale di voti registrati per il partito indicato 
+		return elezioni.get(partito);
 	}
 
 }
