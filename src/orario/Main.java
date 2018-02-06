23898ffd1c94c30f0e479d3e163c53ad3bab47af
RicciLorenzo <lorenzorik@gmail.com>
2018-02-06 18:39:08 +0100

    update

--- src/orario/Main.java
+++ src/orario/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		Model model = new Time();
 		Controller controller = new ControllerImpl(model);
 		model.linkToView(new TextDateView());
-		//model.linkToView(new SwingDateView(controller));
+		model.linkToView(new SwingDateView(controller));
 		model.linkToView(new Swing12DateView(controller));
 	}
 }
